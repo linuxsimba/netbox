73d11e0c666b9ff78daf5a253155a567cd48f2e4
stanley karunditu <stanley@linuxsimba.com>
2016-12-07 22:36:10 -0500

    change swagger code

--- netbox/netbox/urls.py
+++ netbox/netbox/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from rest_framework_swagger.views import get_swagger_view
 from views import home, handle_500, trigger_500
 from users.views import login, logout
 
 
 handler500 = handle_500
+
+schema_view = get_swagger_view(title="Netbox API")
 
 _patterns = [
 
@@ -31,7 +33,7 @@ _patterns = [
     url(r'^api/ipam/', include('ipam.api.urls', namespace='ipam-api')),
     url(r'^api/secrets/', include('secrets.api.urls', namespace='secrets-api')),
     url(r'^api/tenancy/', include('tenancy.api.urls', namespace='tenancy-api')),
-    url(r'^api/docs/', include('rest_framework_swagger.urls')),
+    url(r'^api/docs/', schema_view),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # Error testing
